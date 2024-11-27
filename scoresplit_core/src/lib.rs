4400335f6368ace950e54b45952f3ba3e1c4a699
winzu44 <ftakuabai5000@gmail.com>
2024-11-27 20:18:17 +0900

    Add : get frame from OpenCV and shows on tauri window

--- scoresplit_core/src/lib.rs
+++ scoresplit_core/src/lib.rs
@@ -7,7 +7,7 @@ use opencv::{
 };
 
 /// control streams (camera, video)
-struct StreamManager {
+pub struct StreamManager {
     cam: VideoCapture,
 }
 
@@ -27,6 +27,7 @@ impl StreamManager {
         if self.cam.read(&mut current_frame).is_ok() {
             let mut buf = Vector::<u8>::default();
             let params = Vector::<i32>::default();
+            // encode as jpg, then, convert the image to base64 string
             if imencode(".jpg", &current_frame, &mut buf, &params).is_ok() {
                 let buf_vec = buf.to_vec();
                 let res = general_purpose::STANDARD.encode(&buf_vec);
--- scoresplit_gui/src-tauri/src/lib.rs
+++ scoresplit_gui/src-tauri/src/lib.rs
@@ -1,14 +1,35 @@
+extern crate scoresplit_core;
+
+use std::{thread, time::Instant};
+
+use scoresplit_core::StreamManager;
+use tauri::{window, Emitter, Window};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+/// get frame from captures device(camera, video) using opencv
+fn get_frame(window: Window) {
+    if let Ok(mut stream_manager) = StreamManager::new() {
+        thread::spawn(move || loop {
+            let time = Instant::now();
+            let value = stream_manager.get_frame_as_base64();
+            // emit current frame
+            window.emit("update_frame", value);
+            println!("time : {:?}", time.elapsed());
+        });
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_frame])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
